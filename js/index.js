c1c2bbcf4b9b2853094a1452bceacc6339dc00f1
Oksana Kosyieva <oksankak16@gmail.com>
2019-09-08 17:40:46 +0000

    clean

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 const number=document.querySelectorAll('.calc .number button');
 const display=document.querySelector('.calc .display');
 const action=document.querySelectorAll('.calc .action button');
-const result=document.querySelector('.calc .result button');
-number.forEach(buttonNumber=>buttonNumber.addEventLister('click', numPress));
+const clean=document.querySelector('.calc .clean');
+const result=document.querySelector('.calc .result');
+number.forEach(buttonNumber => buttonNumber.addEventListener('click', numPress) );
 function numPress(ev){
-const number=ev.target.innerText;
+const num= ev.target.innerText;
+display.value+=num;
 }
+action.forEach(buttonAction=>buttonAction.addEventListener('click', actionPress));
+function actionPress(ev){
+//     for(let i = 0; i<action.length; i++){
+//      if(!display.value.includes(action[i].innerText))
+//    {
+    const act=ev.target.innerText;
+    display.value+=act;
+    }
+// }
+
+// }
+clean.addEventListener('click',()=>display.value='');
+result.addEventListener('click',()=>display.value=eval(display.value) );
